7b4fdd8e51b8b08afb032629ad2313bd43d3c6a7
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-04-21 11:16:53 -0300

    feat: cardData | PageTitle | Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,49 @@
+import PageTitle from "@/components/PageTitle";
+import Card, { CardContent, CardProps } from "@/components/Card";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+
+const cardData: CardProps[] = [
+  {
+    label: "Total Revenue",
+    amount: "$45,231.89",
+    description: "+20.1% from last month",
+    icon: DollarSign,
+  },
+  {
+    label: "Subscriptions",
+    amount: "+2350",
+    description: "+180.1% from last month",
+    icon: Users,
+  },
+  {
+    label: "Sales",
+    amount: "+12,234",
+    description: "+19% from last month",
+    icon: CreditCard,
+  },
+  {
+    label: "Active Now",
+    amount: "+573",
+    description: "+201 since last hour",
+    icon: Activity,
+  },
+];
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="flex flex-col gap-5 w-full">
+      <PageTitle title="Dashboard" />
+      <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-4">
+        {cardData.map((d, i) => (
+          <Card
+            key={i}
+            amount={d.amount}
+            description={d.description}
+            icon={d.icon}
+            label={d.label}
+          />
+        ))}
+      </section>
+    </div>
+  );
 }
