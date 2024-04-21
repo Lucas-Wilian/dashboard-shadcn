a442f5e1786484e2fc20114a56d4cb207c02804f
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-04-21 12:14:01 -0300

    add new componet home page, Barchart | SalesCartd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import PageTitle from "@/components/PageTitle";
 import Card, { CardContent, CardProps } from "@/components/Card";
 import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+import BarChart from "@/components/BarChart";
+import SalesCard, { SalesProps } from "@/components/SalesCard";
 
 const cardData: CardProps[] = [
   {
@@ -29,6 +32,34 @@ const cardData: CardProps[] = [
   },
 ];
 
+const uesrSalesData: SalesProps[] = [
+  {
+    name: "Olivia Martin",
+    email: "olivia.martin@email.com",
+    saleAmount: "+$1,999.00",
+  },
+  {
+    name: "Jackson Lee",
+    email: "isabella.nguyen@email.com",
+    saleAmount: "+$1,999.00",
+  },
+  {
+    name: "Isabella Nguyen",
+    email: "isabella.nguyen@email.com",
+    saleAmount: "+$39.00",
+  },
+  {
+    name: "William Kim",
+    email: "will@email.com",
+    saleAmount: "+$299.00",
+  },
+  {
+    name: "Sofia Davis",
+    email: "sofia.davis@email.com",
+    saleAmount: "+$39.00",
+  },
+];
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-5 w-full">
@@ -43,6 +74,28 @@ export default function Home() {
             label={d.label}
           />
         ))}
+      </section>
+      <section className="grid grid-cols-1 gap-4 transition-all lg:grid-cols-2">
+        <CardContent>
+          <p className="p-4 font-semibold">Overview</p>
+          <BarChart />
+        </CardContent>
+        <CardContent>
+          <section>
+            <p>Recent Sales</p>
+            <p className="text-sm text-gray-400">
+              You made 265 sales this month
+            </p>
+          </section>
+          {uesrSalesData.map((d, i) => (
+            <SalesCard
+              key={i}
+              email={d.email}
+              name={d.name}
+              saleAmount={d.saleAmount}
+            />
+          ))}
+        </CardContent>
       </section>
     </div>
   );
