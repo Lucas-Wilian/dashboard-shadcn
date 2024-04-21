8b9396059cc1a0b29de56e062b2e67ea18c40d24
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-04-21 09:38:38 -0300

    add SideNavBar layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import SideNavbar from "@/components/SideNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           }
         )}
       >
+        <SideNavbar />
         <div> {children}</div>
       </body>
     </html>
